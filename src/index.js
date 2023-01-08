9168fa79ae6e49b545f053284026f7fcd4d784f5
NilianFox <wizzy_fox@mail.ru>
2023-01-08 13:20:02 +0700

    feat: reverse function done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let str=Math.abs(n).toString();
+    let result=''
+    for (let i=0;i<str.length;i++){
+        result=str[i]+result;
+    }
+    return Number(result)
 }
